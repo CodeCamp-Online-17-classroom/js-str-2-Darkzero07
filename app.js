a71f24bd59d18c8ad247f8b48a8f8036234ab7c2
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-27 06:30:13 +0700

    js lab 82

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// write code here
+let str = "Welcome to Thailand";
+let str1 = "Don'n be afraid\n";
+let str2 = "สวัสดีประเทศไทย";
+
+console.log(str.length)
+console.log(str1.length)
+console.log(str2.length)
